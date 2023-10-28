b91cac5dac07fb4ec4f69302586e496b1b7e8aeb
Nicole Lee <70028306+nnicolee@users.noreply.github.com>
2023-10-28 16:40:31 -0700

    Initial Commit

---
+++ travelpt2/travelpt2App.swift
@@ -0,0 +1,20 @@
+//
+//  travelpt2App.swift
+//  travelpt2
+//
+//  Created by Nicole Lee on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct travelpt2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
