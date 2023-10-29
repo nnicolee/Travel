9f201b21cc7aaeb62c1c60904eeb74c3f150856e
angiebaik <23angieb@gmail.com>
2023-10-29 05:57:48 -0700

    adjusted indivplan

---
+++ travelpt2/IndivPlan.swift
@@ -0,0 +1,18 @@
+//
+//  IndivPlan.swift
+//  travelpt2
+//
+//  Created by Angie Baik on 10/29/23.
+//
+
+import SwiftUI
+
+struct IndivPlan: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IndivPlan()
+}
