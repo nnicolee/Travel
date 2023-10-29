64fa2e8647bae294bbd6b2c4454d6c901716ccf9
angiebaik <23angieb@gmail.com>
2023-10-29 02:45:43 -0700

    added nav bar

---
+++ travelpt2/Nav.swift
@@ -0,0 +1,18 @@
+//
+//  Nav.swift
+//  travelpt2
+//
+//  Created by Angie Baik on 10/29/23.
+//
+
+import SwiftUI
+
+struct Nav: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Nav()
+}
---
+++ travelpt2/navBar.swift
@@ -0,0 +1,18 @@
+//
+//  navBar.swift
+//  travelpt2
+//
+//  Created by Angie Baik on 10/29/23.
+//
+
+import SwiftUI
+
+struct navBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    navBar()
+}
